8f3366dbed2f9d980a428f7505b589902b5b0cd7
Bruno Martins <bruno.martins217@gmail.com>
2021-05-09 00:26:52 -0300

    refactoring useEffect

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,27 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokeContext from './PokeContext';
 
 function Provider({ children }) {
   const [pokemon, setPokemon] = useState([]);
   const [input, setInput] = useState('');
-  const [pokeName, setPokeName] = useState()
-  const [loading, setLoading] = useState(false)
+  const [pokeName, setPokeName] = useState();
+  const [loading, setLoading] = useState(false);
 
-    function handleChange({ target }) {
+  function handleChange({ target }) {
     setInput(target.value);
   }
 
   function handleClick(input) {
-    setLoading(true)
-    fetch(`https://api.pokemontcg.io/v1/cards?name=${input}`).then((response) => response.json()).then((data) =>{
-      setPokemon([data])
-      setLoading(false)
-    })
-    setInput('')
-    setPokeName(input)
+    setLoading(true);
+    setPokeName(input);
   }
-  console.log(loading)
-  const context = { pokemon, setPokemon, input, setInput, handleChange, handleClick, pokeName, loading };
+
+  useEffect(() => {
+    if(loading) {
+      fetch(`https://api.pokemontcg.io/v1/cards?name=${input}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemon(data.cards);
+        setLoading(false);
+          setInput('');
+      });
+    }    
+  }, [input, loading]);
+
+  const context = {
+    pokemon,
+    setPokemon,
+    input,
+    setInput,
+    handleChange,
+    handleClick,
+    pokeName,
+    loading,
+  };
   return (
     <PokeContext.Provider value={context}>{children}</PokeContext.Provider>
   );
